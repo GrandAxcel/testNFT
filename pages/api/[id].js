54a3d503855f986ed855b40f462ba42354e97c50
GrandAxcel <88810748+GrandAxcel@users.noreply.github.com>
2021-08-27 16:37:18 -0400

    Update [id].js

--- pages/api/[id].js
+++ pages/api/[id].js
@@ -2,7 +2,7 @@ import {INFURA_ADDRESS, ADDRESS, ABI} from "../../config.js"
 import Web3 from "web3";
 
 // import the json containing all metadata. not recommended, try to fetch the database from a middleware if possible, I use MONGODB for example
-import traits from "../../database/finaltraits.json";
+import traits from "https://gateway.pinata.cloud/ipfs/QmYQU1uVdEioAe25J2298rkepASXV32VBtip9i15bNNYho";
 
 const infuraAddress = INFURA_ADDRESS
 
